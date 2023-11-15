43ed4822902987af75ebd0820c3108095310b574
GuiFranBento <142411766+GuiFranBento@users.noreply.github.com>
2023-11-15 12:48:12 -0300

    Jsx, Componentes

---
+++ meu-projeto/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const name = 'Matheus'
+  
+  function sum (a,b) {
+    return a + b 
+  }
+
+  const url = 'https://via.placeholder.com/150'
+  return (
+    <div className="App">
+      <h2>Alterando o JSX</h2>
+      <p>Olá, {name}</p>
+      <p>Soma:{sum(1,2)}</p>
+      <img src={url} alt='Minhas Imagens'/>
+    </div>
+  );
+}
+
+export default App;
